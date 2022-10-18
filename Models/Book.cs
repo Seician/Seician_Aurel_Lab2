1f2174b2e64dff77e9a57f5d81d64c6d2e3185ab
Seician <seician09@yahoo.com>
2022-10-18 16:57:41 +0300

    Adding author class

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,7 +11,8 @@ namespace Seician_Aurel_Lab2.Models
        
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        public string Author { get; set; }
+
+        public Author? Author { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
