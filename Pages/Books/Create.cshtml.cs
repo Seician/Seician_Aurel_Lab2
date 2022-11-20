236c3cd9db39638f967414216e5a7eaa3ecd6d70
Seician <seician09@yahoo.com>
2022-11-20 11:11:22 +0200

    Lab 6

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +12,7 @@ using Seician_Aurel_Lab2.Models;
 
 namespace Seician_Aurel_Lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel :  BookCategoriesPageModel
     {
         private readonly Seician_Aurel_Lab2.Data.Seician_Aurel_Lab2Context _context;
--- Program.cs
+++ Program.cs
@@ -3,9 +3,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Seician_Aurel_Lab2.Data;
 using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy =>
+   policy.RequireRole("Admin"));
+});
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+    options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+});
 builder.Services.AddDbContext<Seician_Aurel_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Seician_Aurel_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Seician_Aurel_Lab2Context' not found.")));
 builder.Services.AddDbContext<LibraryIdentityContext>(options =>
@@ -15,17 +26,12 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 options.SignIn.RequireConfirmedAccount = true)
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
-builder.Services.AddRazorPages(options =>
-{
-    options.Conventions.AuthorizeFolder("/Books");
-    options.Conventions.AllowAnonymousToPage("/Books/Index");
-    options.Conventions.AllowAnonymousToPage("/Books/Details");
 
-});
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
